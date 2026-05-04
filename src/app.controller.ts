448b0f45d4846b26418d91755c2f36e219433c70
Bradley A. Motlhaleemang <bmotlhaleemang@bitri.co.bw>
2026-05-04 10:20:04 +0200

    Commnet added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+//getting started
   @Get()
   getHello(): string {
     return this.appService.getHello();
